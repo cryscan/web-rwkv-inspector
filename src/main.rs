7acf99a014b87fc987e1a653b77db2ba75532550
cedruszhang <cedruszhang@tencent.com>
2024-04-25 18:38:43 +0800

    Load model runtime.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-use std::time::Duration;
+use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
+use half::f16;
+use memmap2::Mmap;
+use safetensors::SafeTensors;
+use tokio::fs::File;
 use web_rwkv::{
     context::{Context, ContextBuilder, Instance},
-    runtime::model::{ContextAutoLimits, ModelInfo},
+    runtime::{
+        infer::{InferInput, InferOutput},
+        loader::Loader,
+        model::{
+            AsAny, Build, ContextAutoLimits, ModelBuilder, ModelInfo, ModelRuntime, ModelVersion,
+        },
+        v6, JobRuntime,
+    },
     wgpu,
 };
 
-async fn _create_context(info: &ModelInfo) -> Result<Context> {
+#[derive(Debug, Clone)]
+struct Runtime {
+    runtime: JobRuntime<InferInput, InferOutput<f16>>,
+    model: v6::Model,
+    state: v6::State,
+}
+
+async fn create_context(info: &ModelInfo) -> Result<Context> {
     let instance = Instance::new();
     let adapter = instance
         .adapter(wgpu::PowerPreference::HighPerformance)
@@ -19,51 +37,91 @@ async fn _create_context(info: &ModelInfo) -> Result<Context> {
     Ok(context)
 }
 
-struct App {
-    name: String,
-    age: u32,
+async fn load_runtime(path: PathBuf) -> Result<Runtime> {
+    let file = File::open(path).await?;
+    let data = unsafe { Mmap::map(&file)? };
+
+    let model = SafeTensors::deserialize(&data)?;
+    let info = Loader::info(&model)?;
+    log::info!("{:#?}", info);
+
+    if !matches!(info.version, ModelVersion::V6) {
+        bail!("only supports V6 models");
+    }
+
+    let context = create_context(&info).await?;
+    log::info!("{:#?}", context.adapter.get_info());
+
+    let builder = ModelBuilder::new(&context, model);
+    let model = Build::<v6::Model>::build(builder).await?;
+
+    let builder = v6::ModelJobBuilder::new(model.clone(), 1);
+    let state = builder.state();
+    let state = state.as_any().downcast_ref::<v6::State>().unwrap().clone();
+    let runtime = JobRuntime::new(builder).await;
+
+    Ok(Runtime {
+        runtime,
+        model,
+        state,
+    })
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
-    }
+#[derive(Debug, Clone)]
+enum App {
+    Start {
+        load_sender: flume::Sender<(PathBuf, flume::Sender<Result<Runtime>>)>,
+    },
+    Loading {
+        path: PathBuf,
+        runtime_receiver: flume::Receiver<Result<Runtime>>,
+        load_sender: flume::Sender<(PathBuf, flume::Sender<Result<Runtime>>)>,
+    },
+    Loaded(Runtime),
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            match self.clone() {
+                App::Start { load_sender } => {
+                    if ui.button("Open file...").clicked() {
+                        if let Some(path) = rfd::FileDialog::new().pick_file() {
+                            let (runtime_sender, runtime_receiver) = flume::unbounded();
+                            let _ = load_sender.send((path.clone(), runtime_sender));
+                            *self = App::Loading {
+                                path,
+                                runtime_receiver,
+                                load_sender,
+                            };
+                        }
+                    }
+                }
+                App::Loading {
+                    path,
+                    runtime_receiver,
+                    load_sender,
+                } => {
+                    ui.label(format!("Loading model from {}...", path.to_string_lossy()));
 
-            // ui.image(egui::include_image!(
-            //     "../../../crates/egui/assets/ferris.png"
-            // ));
+                    if let Ok(runtime) = runtime_receiver.try_recv() {
+                        match runtime {
+                            Ok(runtime) => *self = App::Loaded(runtime),
+                            Err(err) => {
+                                log::error!("{}", err);
+                                *self = App::Start { load_sender };
+                            }
+                        }
+                    }
+                }
+                App::Loaded(runtime) => {}
+            };
         });
     }
 }
 
 #[tokio::main]
-async fn run() -> Result<()> {
-    loop {
-        log::info!("tick");
-        tokio::time::sleep(Duration::from_secs(1)).await;
-    }
-}
-
-fn main() -> Result<(), eframe::Error> {
+async fn main() {
     simple_logger::SimpleLogger::new()
         .with_level(log::LevelFilter::Warn)
         .with_module_level("web_rwkv", log::LevelFilter::Info)
@@ -71,11 +129,22 @@ fn main() -> Result<(), eframe::Error> {
         .init()
         .unwrap();
 
-    std::thread::spawn(run);
+    let (load_sender, load_receiver) = flume::unbounded();
+    let app = App::Start { load_sender };
+
+    tokio::spawn(async move {
+        loop {
+            let Ok((path, sender)) = load_receiver.recv_async().await else {
+                continue;
+            };
+            let runtime = load_runtime(path).await;
+            let _ = sender.send(runtime);
+        }
+    });
 
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
         ..Default::default()
     };
-    eframe::run_native("My egui App", options, Box::new(|_| Box::<App>::default()))
+    eframe::run_native("Web-RWKV Inspector", options, Box::new(|_| Box::new(app))).unwrap();
 }
